c32a1fe2459dea9918cbb67eaedaf15cc14feda2
robinyang11 <756517553@qq.com>
2025-01-16 18:36:26 +0800

    feat:fix

--- langchain_demo/5.prompt_template_with_chat_model.py
+++ langchain_demo/5.prompt_template_with_chat_model.py
@@ -1,6 +1,6 @@
 
 from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 model = ChatOpenAI(
     base_url="https://api.deepseek.com/v1",  
@@ -17,3 +17,17 @@ print(result.content)
 
 
 
+template_multiple = """
+    You are helpful assistant.
+    Human: Tell me a {adjective} short story about a . {animal}.
+    Asssistant:
+"""
+prompt_template_m = ChatPromptTemplate.from_template(template_multiple)
+
+promptm = prompt_template_m.invoke({
+  "adjective":"funny"  ,
+  "animal":"dog"
+})
+
+result1 = model.invoke(promptm)
+print(result1.content)
