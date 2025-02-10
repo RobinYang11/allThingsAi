80e07abbbe3189c3b495246c73018d69adcfb618
robinyang11 <756517553@qq.com>
2025-02-10 18:29:58 +0800

    feat:fix

---
+++ langchain_course/rag/4.c.py
@@ -0,0 +1,39 @@
+from  dotenv import load_dotenv
+
+from langchain.chains import create_history_aware_retriever ,create_retriever_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+# from langchain_community.vectorstores import Chroma
+
+from langchain_chroma import Chroma
+from langchain_core.messages import HumanMessage,SystemMessage
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain_openai import ChatOpenAI,OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
+import os
+
+load_dotenv()
+
+dir  = os.path.dirname(os.path.abspath(__file__))
+persistent_directory = os.path.join(dir,"db","chroma_db_with_metadata")
+
+embeddings = OllamaEmbeddings(
+    base_url="http://127.0.0.1:11434",
+    model="bge-m3"
+)
+
+db = Chroma(persist_directory=persistent_directory,embedding_function=embeddings)
+
+
+
+
+
+
+
+
+
+
+    
+
+create_history_aware_retriever()
+    
